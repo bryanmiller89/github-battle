e29dacbca8c225b8692ebd954e78fcc456089bfd
Bryan Miller <bryanmiller89@gmail.com>
2018-05-21 19:51:17 -0700

    Create App component

---
+++ app/components/App.js
@@ -0,0 +1,16 @@
+var React = require('react');
+
+// Create a React component
+class App extends React.Component {
+  // Component properties: state, lifecycle event, UI
+  // Render method describes component UI
+  render() {
+    return (
+      <div>
+        Hello World
+      </div>
+    )
+  }
+}
+
+module.exports = App;
--- app/index.js
+++ app/index.js
@@ -2,18 +2,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
-
-// Create a React component
-class App extends React.Component {
-  // Component properties: state, lifecycle event, UI
-  render() {
-    return (
-      <div>
-        Hello World
-      </div>
-    )
-  }
-}
+var App = require('./components/App');
 
 ReactDOM.render(
   <App />,
